47b1a71a2a99d9cfc04d1e3e6bcbb68bb960726f
Tasin Ahmmed <122511332+BANTUBD@users.noreply.github.com>
2023-03-23 06:31:45 +0600

    Add files via upload

---
+++ FOF.py
@@ -0,0 +1,5 @@
+import os, platform, time, sys
+os.system('git pull')
+print('[\033[1;32m✓\033[1;37m] Collecting Modules Please Wait ! ')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+import FOF
