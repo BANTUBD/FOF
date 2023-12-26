3f96c7a22d3e49554cda9814debd2b156b5f31bc
Tasin Ahmmed <122511332+BANTUBD@users.noreply.github.com>
2023-12-26 10:20:32 +0600

    Update FR.py

--- FR.py
+++ FR.py
@@ -1,4 +1,4 @@
 import os, platform, time, sys
-print(' \x1b[38;5;121m [●] OPENING RANDOM TOOL')
+print('\x1b[38;5;121m [●] OPENING RANDOM TOOL')
 os.system('git pull')
-import FR_enc
+import NR_enc
